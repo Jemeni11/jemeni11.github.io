0348af0dc2a229971e4aaf9d2a50325bc2e90c37
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2022-06-18 02:00:06 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+let firstNameValid = "First Name cannot be empty"
+let lastNameValid = "Last Name cannot be empty"
+let emailValid = "Looks like this is not an email"
+let passwordValid = "Password cannot be empty"
